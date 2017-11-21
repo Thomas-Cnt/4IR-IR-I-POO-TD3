b470f8b608b93fc67e8919a517c056cc276d5ab7
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-21 14:05:22 +0100

    -- initial commit

---
+++ src/main/java/com/thomascantie/insa/calculateurprix/Country.java
@@ -0,0 +1,30 @@
+package com.thomascantie.insa.calculateurprix;
+
+public enum Country {
+    BELGIQUE("Belgique"),
+    DANEMARK("Danemark"),
+    ALLEMAGNE("Allemagne"),
+    GRECE("Grèce"),
+    ESPAGNE("Espagne"),
+    FRANCE("France"),
+    IRLANDE("Irlande"),
+    ITALIE("Italie"),
+    CHYPRE("Chypre"),
+    LUXEMBOURG("Luxembourg"),
+    PAYS_BAS("Pays-Bas"),
+    AUTRICHE("Autriche"),
+    PORTUGAL("Portugal"),
+    SUEDE("Suède");
+
+    private String text;
+
+    Country(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
+
+}
