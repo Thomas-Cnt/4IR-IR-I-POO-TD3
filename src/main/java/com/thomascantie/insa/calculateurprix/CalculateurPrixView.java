114ff795088469d14cc421c083278056c4d5ad9c
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-24 10:55:42 +0100

    -- add comments

--- src/main/java/com/thomascantie/insa/calculateurprix/CalculateurPrixView.java
+++ src/main/java/com/thomascantie/insa/calculateurprix/CalculateurPrixView.java
@@ -21,10 +21,18 @@ public class CalculateurPrixView extends JFrame {
     public CalculateurPrixView() throws HeadlessException {
         super("Calculateur de prix");
 
+        // association de la fenêtre au controleur
         this.presenter = new CalculateurPrixPresenter(this);
 
+
+        /*
+         * Création des différents composants
+         */
+
+        // panneau pour les champs de saisie
         JPanel pLeft = new JPanel(new SpringLayout());
 
+        /* prix de l'article */
         JLabel labelPrixArticle = new JLabel("Prix d'un article (€) : ", JLabel.TRAILING);
         pLeft.add(labelPrixArticle);
         JTextField fieldPrixArticle = new JTextField(10);
@@ -35,6 +43,7 @@ public class CalculateurPrixView extends JFrame {
         pLeft.add(Box.createVerticalGlue());
         pLeft.add(Box.createVerticalGlue());
 
+        /* quantité d'articles */
         JLabel labelQuantite = new JLabel("Quantité : ", JLabel.TRAILING);
         pLeft.add(labelQuantite);
         JTextField fieldQuantite = new JTextField(10);
@@ -45,6 +54,7 @@ public class CalculateurPrixView extends JFrame {
         pLeft.add(Box.createVerticalGlue());
         pLeft.add(Box.createVerticalGlue());
 
+        /* choix du pays */
         JLabel labelPays = new JLabel("Pays : ", JLabel.TRAILING);
         pLeft.add(labelPays);
         JComboBox<Country> comboPays = new JComboBox<Country>();
@@ -61,6 +71,7 @@ public class CalculateurPrixView extends JFrame {
         pLeft.add(Box.createVerticalGlue());
         pLeft.add(Box.createVerticalGlue());
 
+        /* montant hors taxes */
         JLabel labelMontantHT = new JLabel("Montant HT : ", JLabel.TRAILING);
         pLeft.add(labelMontantHT);
         this.fieldMontantHT = new JFormattedTextField(NumberFormat.getCurrencyInstance());
@@ -72,6 +83,7 @@ public class CalculateurPrixView extends JFrame {
         pLeft.add(Box.createVerticalGlue());
         pLeft.add(Box.createVerticalGlue());
 
+        /* montant toutes taxes comprises */
         JLabel labelMontantTTC = new JLabel("Montant TTC : ", JLabel.TRAILING);
         pLeft.add(labelMontantTTC);
         this.fieldMontantTTC = new JFormattedTextField(NumberFormat.getCurrencyInstance());
@@ -80,12 +92,13 @@ public class CalculateurPrixView extends JFrame {
         labelMontantTTC.setLabelFor(this.fieldMontantTTC);
         pLeft.add(this.fieldMontantTTC);
 
-        SpringUtilities.makeCompactGrid(pLeft,
-                9, 2,          //rows, cols
-                6, 6,        //initX, initY
-                6, 6);       //xPad, yPad
+        SpringUtilities.makeCompactGrid(
+                pLeft,      // component
+                9, 2,       // rows, cols
+                6, 6,       // initX, initY
+                6, 6);      // xPad, yPad
 
-
+        
         JPanel pRight = new JPanel();
         pRight.setLayout(new BoxLayout(pRight, BoxLayout.X_AXIS));
 
@@ -93,10 +106,14 @@ public class CalculateurPrixView extends JFrame {
 
         pRight.add(new JSeparator(SwingConstants.VERTICAL));
 
+        /* calcul des montants */
         JButton buttonCompute = new JButton("Calculer");
         buttonCompute.addActionListener(e -> this.presenter.onComputeButtonClicked(new Article(fieldPrixArticle.getText(), fieldQuantite.getText(), this.country)));
         pRight.add(buttonCompute);
 
+        /*
+         * Ajout des composants à la fenêtre
+         */
 
         JPanel contentPane = new JPanel();
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS ));
