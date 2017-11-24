6734cbb8b0c772f9c84994f9d1df886f465dc03a
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-24 10:43:50 +0100

    -- refactor using Article class

--- src/main/java/com/thomascantie/insa/calculateurprix/CalculateurPrixPresenter.java
+++ src/main/java/com/thomascantie/insa/calculateurprix/CalculateurPrixPresenter.java
@@ -1,35 +1,6 @@
 package com.thomascantie.insa.calculateurprix;
 
-import com.thomascantie.insa.util.Country;
-
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.thomascantie.insa.util.Country.*;
-
 public class CalculateurPrixPresenter {
-
-    private static final Map<Country, Double> TAXES;
-    static {
-        Map<Country, Double> map = new HashMap<Country, Double>();
-        map.put(BELGIQUE,   0.21);
-        map.put(DANEMARK,   0.25);
-        map.put(ALLEMAGNE,  0.19);
-        map.put(GRECE,      0.23);
-        map.put(ESPAGNE,    0.21);
-        map.put(FRANCE,     0.20);
-        map.put(IRLANDE,    0.23);
-        map.put(ITALIE,     0.22);
-        map.put(CHYPRE,     0.19);
-        map.put(LUXEMBOURG, 0.15);
-        map.put(PAYS_BAS,   0.21);
-        map.put(AUTRICHE,   0.20);
-        map.put(PORTUGAL,   0.23);
-        map.put(SUEDE,      0.25);
-        TAXES = Collections.unmodifiableMap(map);
-    }
 
     private final CalculateurPrixView calculateurPrixView;
 
@@ -37,26 +8,20 @@ public class CalculateurPrixPresenter {
         this.calculateurPrixView = calculateurPrixView;
     }
 
-    public void onComputeButtonClicked(String montantArticleAsText, String quantiteArticleAsText, Country country) {
-        if (!montantArticleAsText.matches("[0-9]+(.[0-9]{1,2})?"))
+    public void onComputeButtonClicked(Article article) {
+        if (!article.hasValidMontant())
             this.calculateurPrixView.afficherErreur("Le montant n'est pas valide !");
-        else if (!quantiteArticleAsText.matches("[0-9]+"))
+        else if (!article.hasValidQuantite())
             this.calculateurPrixView.afficherErreur("La quantité n'est pas valide !");
         else
-            this.compute(montantArticleAsText, quantiteArticleAsText, country);
+            this.displayMontants(article);
     }
 
-    private void compute(String montantArticleAsText, String quantiteArticleAsText, Country country) {
-        double montant = Float.parseFloat(montantArticleAsText);
-        int quantite = Integer.parseInt(quantiteArticleAsText);
+    private void displayMontants(Article article) {
 
-        BigDecimal res = new BigDecimal(montant);
-        res = res.multiply(new BigDecimal(quantite));
+        this.calculateurPrixView.afficherMontantHT(article.getMontantHT());
 
-        this.calculateurPrixView.afficherMontantHT(res.setScale(2, BigDecimal.ROUND_HALF_EVEN).doubleValue());
-
-        res = res.add(res.multiply(new BigDecimal(TAXES.get(country))));
-
-        this.calculateurPrixView.afficherMontantTTC(res.setScale(2, BigDecimal.ROUND_HALF_EVEN).doubleValue());
+        this.calculateurPrixView.afficherMontantTTC(article.getMontantTTC());
     }
+
 }
